ec3586ff6852f48353d405cae80bfca8c1ca36d9
uknow-mac <yoon6763@naver.com>
2025-07-28 12:31:58 +0900

    Feat: ThemeData 분리

---
+++ lib/app_theme.dart
@@ -0,0 +1,23 @@
+// lib/app_theme.dart
+
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.lightBlue,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.lightBlue,
+      foregroundColor: Colors.white,
+    ),
+    colorScheme: const ColorScheme.light(
+      surface: Colors.white,
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Colors.lightBlueAccent,
+    ),
+    useMaterial3: true,
+  );
+
+  AppTheme._();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lecture/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      title: "Flutter D' Class",
+      theme: AppTheme.lightTheme,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -40,10 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: Text(widget.title)),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
